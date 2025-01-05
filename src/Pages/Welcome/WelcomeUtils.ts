cdbdf07c30a77e14aa14a8273b2623426c3f049a
Benjamin Collingwood <benjamincollingwood@me.com>
2025-01-05 19:00:54 +0000

    change utils

--- src/Pages/Welcome/WelcomeUtils.ts
+++ src/Pages/Welcome/WelcomeUtils.ts
@@ -1,9 +1,9 @@
 export const nextSession = (nextSession?: string) => {
   if (nextSession)
-    return new Date(nextSession).toLocaleString(undefined, {
+    return new Date(nextSession).toLocaleDateString("en-gb", {
       dateStyle: "full",
     });
   const d = new Date();
   d.setDate(d.getDate() + ((3 + 7 - d.getDay()) % 7));
-  return d.toLocaleDateString(undefined, { dateStyle: "full" });
+  return d.toLocaleDateString("en-gb", { dateStyle: "full" });
 };
